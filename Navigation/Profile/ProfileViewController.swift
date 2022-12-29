ac56354d936634242b7235168a86a50d465450a3
Pavel Grigorev <amorfien@yandex.ru>
2022-12-29 16:02:40 +0300

    del some unnecessary code

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -27,7 +27,6 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
         view.addSubview(Self.tableView)
         Self.tableView.dataSource = self
         Self.tableView.delegate = self
