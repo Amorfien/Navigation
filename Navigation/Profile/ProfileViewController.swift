88fb51915d0c5815b7d344771d4447ecec37ac36
Pavel Grigorev <amorfien@yandex.ru>
2023-01-10 15:57:00 +0300

    #1 SnapKit

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -27,6 +27,7 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemGray3
         view.addSubview(Self.tableView)
         Self.tableView.dataSource = self
         Self.tableView.delegate = self
